f3f4c30d0d3a842675467907227ebfe3c78ffc89
khengkok <khengkok@gmail.com>
2024-10-20 02:17:29 +0800

    updated

--- benchmark.py
+++
@@ -1,7 +0,0 @@
-from ultralytics.utils.benchmarks import benchmark
-from ultralytics import YOLO
-
-model = YOLO('best.pt')
-model.export(format="openvino")
-# # Benchmark on GPU
-# benchmark(model="best.pt", data="datasets/data.yaml", imgsz=640, half=False, device=0)
--- live_detect.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11s.pt", task="detection")
-model.predict(source=0, show=True)
-
